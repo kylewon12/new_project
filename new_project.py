81216516727afff14fdc37f2965497453fa14c76
Kyle Won <kwon12@terpmail.umd.edu>
2024-11-08 10:19:52 -0500

    Added display method

--- new_project.py
+++ new_project.py
@@ -10,16 +10,24 @@ print("Melat Abera")
 print("Bereket Zeggai")
 
 
-# this can be deleted later, just wanted to make something random ;p
-print("\n\t---- NUMBER GUESSER ----")
-usr_num = input("Enter a numbers between 1-3: ")
+game_board = {"kyle":[{"number":1,"suit":"hearts","player":"kyle"},{"number":3,"suit":"hearts","player":"kyle"},{"number":5,"suit":"spades","player":"kyle"}],
+               "bill":[{"number":3,"suit":"hearts","player":"bill"},{"number":2,"suit":"hearts","player":"bill"},{"number":10,"suit":"diamonds","player":"bill"}],
+               "tim":[{"number":3,"suit":"spades","player":"tim"},{"number":4,"suit":"clubs","player":"tim"},{"number":9,"suit":"diamonds","player":"tim"}]}
 
-n = (r.random() * 3) + 1
-n = math.floor(n)
+# Kyle Won's method
 
+def display(game_board):
+    """
+        Function that goes through each hand and displays them 
+        Args: game_board: Basically a list of all players' hands
+        Side effects: Prints each players deck
+        Returns: none
+    """
+    for player,cards in game_board.items():
+        print(player,"\'s deck:")
+        for card in cards:
+            print(f"{card["number"]} of {card["suit"]}")
 
-if int(usr_num) == n:
-    print(f"LMAO! the computer also guessed {n}, YOU LOST")
-
-else:
-    print("NICE! you won")
+display(game_board)
+            
+        
